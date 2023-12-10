5182ca38c2a8b06c30504def02833813ee5ca75d
victoriakapelush <victoriakapelush@gmail.com>
2023-12-10 00:48:15 -0500

    Fix local storage issue

--- src/index.js
+++ src/index.js
@@ -1,106 +1,48 @@
-/*const Tasks = [];
-
 const section = document.getElementById('section');
 const inputValue = document.getElementById('text');
+const btnDel = document.getElementsByClassName('btnDel');
 
-//add tasks to the array once "enter" is pressed
-inputValue.addEventListener("keyup", function(event) {
-    if (event.key === "Enter") {
-      addTask();
-      Tasks.push(inputValue.value);
-      this.value = "";
-    }
-    addToLocalStorage();
-});
+const taskArray = [];
 
-function addToLocalStorage(task) {
-    // conver the array to string then store it.
-    localStorage.setItem('task', JSON.stringify(Tasks));
-  }
-
-  // function helps to get everything from local storage
-function getFromLocalStorage() {
-        const reference = JSON.parse(localStorage.getItem('task'))
-
-for (let i = 0; i < reference.length; i++) {
-  const div = document.createElement('div');
-  section.appendChild(div);
-  div.setAttribute('class', 'tasks-box');
-  const div1 = document.createElement('div');
-  div.appendChild(div1);
-  div1.setAttribute('class', 'tasks');
-   //create buttons with ticks
-  const btnDel = document.createElement('button');
-  btnDel.setAttribute('class', 'btnDel');
-  div.appendChild(btnDel);
-  btnDel.textContent = "X";
-  div1.textContent = reference[i];
-
-          btnDel.addEventListener("click", function() {
-            for (let i = 0; i < reference.length; i++) {
-            div.remove();
-            localStorage.removeItem('task');
-            const Tasks = [];
-            
-            Tasks.push(section.textContent);
-            localStorage.setItem('task', JSON.stringify(Tasks));
-            
-            }  
-          })
-
-        }}
-    
-
-
-  // initially get everything from localStorage
 getFromLocalStorage();
 
-function addTask() {
-    const div = document.createElement('div');
-    section.appendChild(div);
-    div.setAttribute('class', 'tasks-box');
-    const div1 = document.createElement('div');
-    div.appendChild(div1);
-    div1.setAttribute('class', 'tasks');
-     //create buttons with ticks
-    const btnDel = document.createElement('button');
-    btnDel.setAttribute('class', 'btnDel');
-    div.appendChild(btnDel);
-    btnDel.textContent = "X";
-    div1.textContent = inputValue.value;    
-}*/
-
-
-const tasks = [];
-
-const section = document.getElementById('section');
-const inputValue = document.getElementById('text');
-
 //add tasks to the array once "enter" is pressed
 inputValue.addEventListener("keyup", function(event) {
-    if (event.key === "Enter") {
-      showTask();
-      tasks.push(inputValue.value);
-      this.value = "";
-    }
-    addToLocalStorage();
+  if (event.key === "Enter") {
+    showTask();
+    taskArray.push(inputValue.value);
+    this.value = "";
+  }
+  addToLocalStorage();
 });
 
+//display every single enetred task on the screen
+function showTask() {
+  const div = document.createElement('div');
+  section.appendChild(div);
+  div.setAttribute('class', 'tasks');
+  //create buttons with ticks
+  const btnDel = document.createElement('button');
+  btnDel.setAttribute('class', 'btnDel');
+  section.appendChild(btnDel);
+  btnDel.textContent = "X";
+  div.textContent = inputValue.value;
+}
+
 function addToLocalStorage(task) {
-    // conver the array to string then store it.
-	if (tasks.length === 0) return;
-    localStorage.setItem('task', JSON.stringify(tasks));
-  }
+  // conver the array to string then store it.
+if (taskArray.length === 0) return;
+  localStorage.setItem('task', JSON.stringify(taskArray));
+}
 
-  // function helps to get everything from local storage
+//get everything from local storage
 function getFromLocalStorage() {
-	let item = localStorage.getItem('task');
-	if (item === undefined || item === null) return;
-	
-	tasks = JSON.parse(localStorage.getItem('task'))
-	console.log(tasks)
+  let item = localStorage.getItem('task');
+  if (item === undefined || item === null) return;
+    
+  tasks = JSON.parse(localStorage.getItem('task'))
 
-	for (let i = 0; i < tasks.length; i++) {
+  for (let i = 0; i < tasks.length; i++) {
 	  const div = document.createElement('div');
 	  section.appendChild(div);
 	  div.setAttribute('class', 'tasks');
@@ -111,34 +53,22 @@ function getFromLocalStorage() {
 	  btnDel.textContent = "X";
 	  div.textContent = tasks[i];
 
-	  btnDel.addEventListener("click", function() {
-		for (let i = 0; i < tasks.length; i++) {
-		  div.remove();
-		  btnDel.remove();
-		  localStorage.removeItem('task');
-		}  
-	  })
+//remove a single item from local storage and display the ones left
+    btnDel.addEventListener('click', function() {
+        taskArray.shift();
+        div.remove();
+        btnDel.remove();
+        localStorage.setItem('task', JSON.stringify(taskArray));
+    })
+  }
 
-	}
+//add new items to the existing array in local storage
+  if (localStorage.length !== 0) {
+    for (let i = 0; i < tasks.length; i++) {
+      let newTaskArray = taskArray.push(tasks[i]);
+      console.log(newTaskArray)
+    }
+  }
 }
-    
-
-
-  // initially get everything from localStorage
-getFromLocalStorage();
-
-function showTask() {
-    const div = document.createElement('div');
-    section.appendChild(div);
-    div.setAttribute('class', 'tasks');
-    //create buttons with ticks
-    const btnDel = document.createElement('button');
-    btnDel.setAttribute('class', 'btnDel');
-    section.appendChild(btnDel);
-    btnDel.textContent = "X";
-    div.textContent = inputValue.value;
-    
-}
-
 
 
